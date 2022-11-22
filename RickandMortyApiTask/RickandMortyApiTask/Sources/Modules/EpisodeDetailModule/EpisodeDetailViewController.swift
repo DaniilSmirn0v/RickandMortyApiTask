22e6664242a4a61e4751edfd47c4ae918290a48b
DaniilSmirn0v <d1n.sm1rnv@gmail.com>
2022-11-23 00:48:21 +0600

    feat: Prepare class for layout.

---
+++ RickandMortyApiTask/RickandMortyApiTask/Sources/Modules/EpisodeDetailModule/EpisodeDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EpisodeDetailViewController.swift
+//  RickandMortyApiTask
+//
+//  Created by Даниил Смирнов on 23.11.2022.
+//
+
+import UIKit
+
+class EpisodeDetailViewController: UIViewController {
+    //MARK: - Properties
+
+
+    //MARK: - LifeCycle
+}
