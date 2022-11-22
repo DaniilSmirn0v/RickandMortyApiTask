9e0bf2466de5dcd757b5bf54c42dfff31c67655b
DaniilSmirn0v <d1n.sm1rnv@gmail.com>
2022-11-22 15:42:43 +0600

    feat: Prepare project to work.

--- RickandMortyApiTask/RickandMortyApiTask/Sources/CharactersListModule/CharactersListViewController.swift
+++ RickandMortyApiTask/RickandMortyApiTask/Sources/CharactersListModule/CharactersListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CharactersListViewController.swift
 //  RickandMortyApiTask
 //
 //  Created by Даниил Смирнов on 22.11.2022.
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CharactersListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+
     }
 
 
